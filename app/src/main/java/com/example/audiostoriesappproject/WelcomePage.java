27672657b28df7a472a1b4f354ace100819ccd75
mariaamorgianou <mariaamorgianou.1994@gmail.com>
2024-02-29 02:02:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/audiostoriesappproject/WelcomePage.java
@@ -0,0 +1,24 @@
+package com.example.audiostoriesappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+
+        int delay = 4000;
+
+        new Handler().postDelayed(() -> {
+            // Intent intent = new Intent(WelcomePage.this, MenuPage.class);
+            // startActivity(intent);
+            finish();
+        }, delay);
+    }
+}
