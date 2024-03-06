7feac6e6d2e6f660f5943c1d95ec1521b3b6b437
ATziv <143966821+ATziv@users.noreply.github.com>
2024-03-06 21:26:01 +0200

    The best app is here!

--- app/src/main/java/com/example/audiostoriesappproject/ViewStoriesPage.java
+++ app/src/main/java/com/example/audiostoriesappproject/ViewStoriesPage.java
@@ -1,9 +1,12 @@
 package com.example.audiostoriesappproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
@@ -11,28 +14,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+
 public class ViewStoriesPage extends AppCompatActivity {
     FirebaseDatabase database;
-    DatabaseReference reference1,reference2,reference3,reference4,reference5;
-    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stories_page);
         database = FirebaseDatabase.getInstance();
-        reference1 = database.getReference("Story1");
-        reference2 =database.getReference("Story2");
-        reference3 =database.getReference("Story3");
-        reference4 =database.getReference("Story4");
-        reference5 =database.getReference("Story5");
-        storageReference = FirebaseStorage.getInstance().getReference();
     }
 
-    public void goAladin(View view){
+    public void goAladdin(View view){
         Intent intent = new Intent(this, StoriesPlayerPage.class);
         startActivity(intent);
-        String story = "Aladin";
+        String story = "Aladdin";
         intent.putExtra("mykey1", story);
         startActivity(intent);
     }
@@ -70,6 +67,50 @@ public class ViewStoriesPage extends AppCompatActivity {
         String story = "Jack";
         intent.putExtra("mykey1", story);
         startActivity(intent);
+    }
+    public void recordVoice(View view){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent,123);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode ==123 && resultCode ==RESULT_OK){
+            ArrayList<String>recognizedText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (recognizedText != null && !recognizedText.isEmpty()){
+                String chosenStory = recognizedText.get(0);
+                goToStory(chosenStory);
+            } else {
+                showMessage("Error", "Please choose an existing story from the options");
+            }
+        }
+    }
+    private void goToStory(String recognizedText){
+        Intent intent = new Intent(this,StoriesPlayerPage.class);
+
+        if (recognizedText.contains("Aladdin")){
+            intent.putExtra("mykey1","Aladdin");
+        } else if (recognizedText.contains("Rapunzel")){
+            intent.putExtra("mykey1","Rapunzel");
+        } else if (recognizedText.contains("Snow White")){
+            intent.putExtra("mykey1","Snow White");
+        } else if (recognizedText.contains("Pigs")){
+            intent.putExtra("mykey1","Pigs");
+        } else if (recognizedText.contains("Hansel")){
+            intent.putExtra("mykey1","Hansel");
+        } else if (recognizedText.contains("Jack")){
+            intent.putExtra("mykey1","Jack");
+        }
+        startActivity(intent);
+    }
+
+    public void showMessage(String title, String message){
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setCancelable(true)
+                .show();
     }
 
     public void onBackButtonClick(View view) {
